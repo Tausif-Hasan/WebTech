89a7a7d613518aa0a3310984350de5e544fd31aa
Tausif-Hasan <85207168+Tausif-Hasan@users.noreply.github.com>
2021-10-28 20:19:55 +0600

    Add files via upload

---
+++ Lab Task 4/change_password.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>change_password</title>
+</head>
+<body>
+     <form action="checkChangePass.php" method="POST">
+		<br/>
+		<table border ='1' cellpadding="0" cellspacing="0" >
+			<tr>
+				<td colspan='2'>C company
+					
+			</td>
+			</tr>
+			<tr>		
+				<td>
+				<p><b>Account</b></p>
+				<hr>
+					<ul>
+					<li><a href="dashboard.php">Dashboard</a></li>    
+					<li><a href="profile.php">View Profile</a></li>                             
+					<li><a href="edit_profile.php">Edit Profile</a></li>
+					<li><a href="picture.html">Change Profile Picture</a></li>
+					<li><a href="change_password.php">Change Password</a></li>
+					<li><a href="logout.php">Logout</a></li>
+					</ul>
+                </td>
+                <td> 
+                    <fieldset>
+                        <legend><b>CHANGE PASSWORD</b></legend>
+                        <form>
+                            <table>
+                                <tr>
+                                    <td><font size="3">Current Password</font></td>
+                                    <td>:</td>
+                                    <td><input type="password" name ="curPass" /></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td><font size="3" color="green">New Password</font></td>
+                                    <td>:</td>
+                                    <td><input type="password" name="newPass" /></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td><font size="3" color="red">Confirm New Password</font></td>
+                                    <td>:</td>
+                                    <td><input type="password" name="confPass" /></td>
+                                    <td></td>
+                                </tr>
+                            </table>
+                            <hr />
+                            <input type="submit" name="submit" value="Submit"  />
+                        </form>
+                    </fieldset>
+                </td>
+				</tr>			
+            			</table>		
+    </form>		
+</body>
+</html>
+
+
+
---
+++ Lab task 3/3.4.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <style>
+    </style>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p><h3>Profile Picture</h3></p>
+    <img src="pic.jpg" alt="" width="400" height="400">
+    <br>
+    <form class="" action="upload.php" method="post" enctype="multipart/form-data">
+      <input type="file" name="fileToUpload" id="fileToUpload">
+      <br><br>
+      <input type="submit" value="Submit" name="submit">
+    </form>
+  </body>
+</html>
